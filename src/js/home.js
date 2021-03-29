90db1b1f6e9b92b960b995ae449e09371faff252
Theo Cheron <35193221+Volturuss@users.noreply.github.com>
2021-03-29 14:50:18 +0200

    Update home.js
    Added the generics cards

--- src/js/home.js
+++ src/js/home.js
@@ -1,33 +1,53 @@
 const fetch = require('node-fetch')
 const url = 'https://raw.githubusercontent.com/Volturuss/Projet-6/development_photographer_page-factory/data.json'
-// const container = document.querySelector('#container')
+const container = document.querySelector('#container')
+const main = document.createElement('main')
+const section = document.createElement('section')
+const h1 = document.createElement('h1')
 
-console.log(fetch(url))
-
-/* fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    data.photographers.forEach(p => {
-      container.innerHTML += ` <article class="photographer">
-                      <a href="">
-                          <div class="photographer__idContainer">
-                              <figure class="photographer__idContainer__picture">
-                                  <img src="${p.portrait}" class="user photographer__idContainer__user" alt="">
-                          </figure>
-                          <h2 class="name photographer__idContainer__name">${p.name}</h2>
-                          </div>
-                      </a>
-                      <div class="photographer__container">
-                          <h3 class="location photographer__container__location">${p.city}, ${p.country}</h3>
-                          <p class="description photographer__container__description">${p.tagline}</p>
-                          <small class="photographer__container__price">${p.price}/jour</small>
-                          <ul class="photographer__container__hashtags">
-                              <a href="#"><li class="tag-name photographer__container__tag-name">#${p.tags[0]}</li></a>
-                              <a href="#"><li class="tag-name photographer__container__tag-name">#${p.tags[1]}</li></a>
-                              <a href="#"><li class="tag-name photographer__container__tag-name">#${p.tags[2]}</li></a>
-                              <a href="#"><li class="tag-name photographer__container__tag-name">#${p.tags[3]}</li></a>
-                          </ul>
-                      </div>
-                  </article> `
+const searchPhotographers = () => {
+  fetch(url)
+    .then(res => {
+      if (res.ok) {
+        res.json()
+          .then(data => {
+            console.log(data)
+            data.photographers.forEach(p => {
+              section.innerHTML +=
+              `
+              <article class="photographer">
+              <a href="${p.name.toLowerCase().replace(/\s/g, '')}.html">
+                  <div class="photographer__idContainer">
+                      <figure class="photographer__idContainer__picture">
+                          <img src="../public/assets/images/Photographers_ID_Photos/${p.portrait}" class="user photographer__idContainer__user" alt="">
+                  </figure>
+                  <h2 class="name photographer__idContainer__name">${p.name}</h2>   
+                  </div>
+              </a>
+              <div class="photographer__container">
+                  <h3 class="location photographer__container__location">${p.city}, ${p.country}</h3>
+                  <p class="description photographer__container__description">${p.tagline}</p>
+                  <small class="photographer__container__price">${p.price}/jour</small>
+                  <ul class="photographer__container__hashtags">
+                    <a href="#"><li class="tag-name photographer__container__tag-name">#${p.tags}</li></a>
+                  </ul>
+              </div>
+          </article>
+              `
+            })
+          })
+      } else {
+        console.log('Erreur')
+      }
     })
-  }) */
+}
+
+const buildPhotographers = () => {
+  searchPhotographers()
+}
+
+buildPhotographers()
+document.body.appendChild(container)
+container.appendChild(main)
+main.appendChild(section)
+section.appendChild(h1)
